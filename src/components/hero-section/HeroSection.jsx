e78898952f7200a8afb87e5c2af9d00a69b54658
x-russian <philophilo037@gmail.com>
2023-09-10 16:57:57 +0300

    :lipstick: stylings.

--- src/components/hero-section/HeroSection.jsx
+++ src/components/hero-section/HeroSection.jsx
@@ -3,7 +3,7 @@ import Search from './Search'
 
 const HeroSection = () => {
   return (
-    <div className='hero-section w-full h-96 m-2 bg-blue-400'>
+    <div className='hero-section w-full h-96 m-2 bg-ft2-image bg-blue-400'>
         <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</h1>
         <Search/>
     </div>
