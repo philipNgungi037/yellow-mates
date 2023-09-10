9874aaeed3620cf5f708e6d5575fb85f3a6970f9
x-russian <philophilo037@gmail.com>
2023-09-10 11:47:53 +0300

    :sparkles:feat: render hero-section into a landing-page.

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,14 @@
   import React from 'react'
 import Header from '../components/header/Header'
+import HeroSection from '../components/hero-section/HeroSection'
   
   const LandingPage = () => {
     return (
       <div className='landing-page'>
         <Header/>
+        <div className=''>
+            <HeroSection/>
+        </div>
       </div>
     )
   }
