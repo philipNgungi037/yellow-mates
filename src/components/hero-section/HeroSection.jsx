8c1875c7b4bf9825eff880ec16f7e1bcbe8d33b0
x-russian <philophilo037@gmail.com>
2023-09-10 11:45:42 +0300

    :sparkes: Hero-section component.

---
+++ src/components/hero-section/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Search from './Search'
+
+const HeroSection = () => {
+  return (
+    <div className='hero-section w-full h-96 m-2 bg-blue-400'>
+        <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</h1>
+        <Search/>
+    </div>
+  )
+}
+
+export default HeroSection
