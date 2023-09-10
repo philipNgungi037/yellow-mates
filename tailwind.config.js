7ec81f5459f2bba31a4a0c46536ce6abc7726700
x-russian <philophilo037@gmail.com>
2023-09-10 16:52:06 +0300

    :wrench: tailwindcss configuration.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,42 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,css}"],
   theme: {
-    extend: {},
+    // colors: {
+    //   'grent': '#39edb8"',
+    // },
+    screens: {
+      xs: "480px",
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      backgroundImage:{
+        'fi2-image':'url("./components/images/feat-img2.jpg")',
+      },
+      fontFace: {
+        'fontawesome': {
+          'fontFamily': 'Font Awesome',
+          'src': 'url(/fonts/font-awesome/fonts/fontawesome-webfont.woff) format("woff")',
+          'fontStyle': 'normal',
+          'fontWeight': 'normal',
+          'fontDisplay': 'auto',
+        }
+      },
+      
+    },
   },
   plugins: [],
 }
