cc61398786eb5d2ce7d9d2593e26d651fbe17259
x-russian <philophilo037@gmail.com>
2023-09-10 16:58:48 +0300

    :sparkles:feat: Create features component.

---
+++ src/components/organization/Features.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import imageData from './organization-data'
+import './organization.css'
+
+const Features = () => {
+  return (
+    <div className='features mx-2 '>
+        <h1> Neque porro quisquam est qui dolorem </h1>
+        <div className='features-card flex justify-between gap-0'>
+      {imageData.map((item, index) => (
+        <div key={index}>
+          <img className=' ' src={item.image} alt={item.description} />
+          <p>{item.description}</p>
+        </div>
+      ))}
+    </div>
+    </div>
+  )
+}
+
+export default Features
