022329e288108bd369e35b5a5cb697722d83a1a7
x-russian <philophilo037@gmail.com>
2023-09-10 09:22:16 +0300

    :sparkles: website logo/head.

---
+++ src/components/header/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className='logo p-4 items-center content-center bg-slate-300'>
+        <h1 className=' text-blue-900'>REAL-DEAL HOMES</h1>
+
+    </div>
+  )
+}
+
+export default Logo
