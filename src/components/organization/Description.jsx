324ccfbde928d706a553695ac647e98a7f3285f7
x-russian <philophilo037@gmail.com>
2023-09-12 20:26:42 +0300

    :sparkles: description for the organization.

---
+++ src/components/organization/Description.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Description = () => {
+  return (
+    <div className='description m-2 ' >
+        <h1 className='font-bold text-4xl p-2 text-center '> Nam placerat neque in ultrices placerat</h1>
+        <p>Nam et suscipit lacus, at vehicula lacus. Suspendisse potenti. Praesent scelerisque 
+          urna non quam varius interdum non vel elit. Nulla commodo sollicitudin ex, non eleifend nisl molestie eget. Etiam varius ligula vel posuere venenatis. Mauris sit amet enim in lacus vulputate laoreet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet euismod eros, a pretium nisl. Praesent egestas mauris maximus ultricies fermentum. Nunc at lacinia turpis. Praesent tincidunt ullamcorper orci, eget lacinia sapien ullamcorper sit amet. 
+          Nam placerat neque in ultrices placerat. Phasellus ut quam risus.</p>
+    </div>
+  )
+}
+
+export default Description
