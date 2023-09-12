d51978f09d6bd3f0f5ab8d50d7924937902d8ff2
x-russian <philophilo037@gmail.com>
2023-09-12 19:36:54 +0300

    :sparkles: Add social-image + social-carousel + paragraphs data.

---
+++ src/components/social-proof/CarouselParagraphs.jsx
@@ -0,0 +1,26 @@
+// CarouselParagraphs.jsx
+const carouselParagraphs = [
+    {
+      description: 'This is the first paragraph of the carousel.',
+      name: 'John Kei',
+    },
+    {
+      description: 'Here is the second paragraph that will appear.',
+      name: 'pal mahr',
+    },
+    {
+      name: 'Paragraph 3',
+      description: 'The third paragraph is coming up next.',
+    },
+    {
+      name: 'Paragraph 4',
+      description: 'Fourth paragraph, ready to be displayed.',
+    },
+    {
+      name: 'Paragraph 5',
+      description: 'Last but not least, the fifth paragraph.',
+    },
+  ];
+  
+  export default carouselParagraphs;
+  
