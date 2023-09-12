328a622d932cb8b81fc85b795961954d6fd73e78
x-russian <philophilo037@gmail.com>
2023-09-12 20:29:52 +0300

    :sparkles: cretae blogs mock-data.

---
+++ src/components/blogs/blogData.jsx
@@ -0,0 +1,32 @@
+const blogData = [
+    {
+      id: 1,
+      title: "The Power of Real-estates",
+      author: "John Smith",
+      date: "2023-09-12",
+      content: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Proin vel nulla a justo consequat sodales. Sed auctor libero 
+        eu purus malesuada convallis. Nulla facilisi. 
+        Praesent ac consectetur lectus.
+      `,
+      tags: ["Real-estates", "Family-things", "Homes"],
+    },
+    {
+      id: 2,
+      title: "Exploring the Great Outdoors",
+      author: "Jane Doe",
+      date: "2023-09-10",
+      content: `
+        Vestibulum ante ipsum primis in faucibus orci luctus et 
+        ultrices posuere cubilia Curae; Morbi iaculis cursus augue, 
+        eu vehicula ipsum venenatis at. Nullam sit amet tincidunt 
+        libero, nec fermentum nunc. Sed luctus, elit ac fermentum 
+        sodales, dui lectus pellentesque libero.
+      `,
+      tags: ["Nature", "Travel", "Adventure"],
+    },
+  ];
+  
+  export default blogData;
+  
