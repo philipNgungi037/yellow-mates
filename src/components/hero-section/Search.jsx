1a54dd15bff5213b41b849a020b1402993766fab
x-russian <philophilo037@gmail.com>
2023-09-12 20:17:22 +0300

    :sparkles: search component.

--- src/components/hero-section/Search.jsx
+++ src/components/hero-section/Search.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './search.css'
 
 const Search = () => {
   const [searchQuery, setSearchQuery] = useState('');
@@ -13,14 +14,15 @@ const Search = () => {
   };
 
   return (
-    <div className='search bg-blue-400 m-4'>
-         <button className='   rounded-md text-2xl pb-2' onClick={handleSearch}><ion-icon  name="search-outline"></ion-icon></button>
+    <div className='search m-4 absolute top-2  '>
+         
       <input className='text-center pt-2'
         type="text"
         placeholder="search"
         value={searchQuery}
         onChange={handleSearchChange}
       />
+      <button className='   rounded-md text-2xl pb-2' onClick={handleSearch}><ion-icon  name="search-outline"></ion-icon></button>
     </div>
   );
 };
