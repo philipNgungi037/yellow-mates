b8f55e5ae0699795cb38ca182b6948a435b8ccc6
x-russian <philophilo037@gmail.com>
2023-09-12 19:33:22 +0300

    :sparkles: import + render multiple components into our lainding page.

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,14 +2,27 @@
 import Header from '../components/header/Header'
 import HeroSection from '../components/hero-section/HeroSection'
 import Organization from '../components/organization/Organization'
+import SocialProof from '../components/social-proof/SocialProof'
+import Cta from '../components/cta/Cta'
+import Theme from '../components/Organization-commitment/Theme'
+import Homes from '../components/homes/Homes'
+import BLogs from '../components/blogs/BLogs'
+import Footer from '../components/footer/Footer'
   
   const LandingPage = () => {
     return (
       <div className='landing-page'>
         <Header/>
-        <div className=''>
+        <div className='flex flex-col gap-8  '>
             <HeroSection/>
             <Organization/>
+            <SocialProof/>
+            <Cta/>
+            <Theme/>
+            <Homes/>
+            <BLogs/>
+            <Footer/>
+            
         </div>
       </div>
     )
