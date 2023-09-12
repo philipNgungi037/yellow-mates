55230d12d2422f4987919a033ed8ab7b188d5c03
x-russian <philophilo037@gmail.com>
2023-09-12 19:31:32 +0300

    :sparkles: Import + render search component into hero section.

--- src/components/hero-section/HeroSection.jsx
+++ src/components/hero-section/HeroSection.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Search from './Search'
+import Carousel from './Carousel'
 
 const HeroSection = () => {
   return (
-    <div className='hero-section w-full h-96 m-2 bg-ft2-image bg-blue-400'>
-        <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</h1>
-        <Search/>
+    <div className='hero-section w-full h-96 m-2 bg-ft2-image  relative'>
+        <h1 className='absolute text-center'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</h1>
+       
+        <Search /> 
+        
+        <Carousel/>
+        
     </div>
   )
 }
