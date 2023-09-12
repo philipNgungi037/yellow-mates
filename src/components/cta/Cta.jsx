71a9cf0154cae9ae1341e63e8ccd353513820356
x-russian <philophilo037@gmail.com>
2023-09-12 20:15:29 +0300

    :sparkles: Call in action component + imports home-types and learn-more component.

---
+++ src/components/cta/Cta.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HomeTypes from '../homes/HomeTypes'
+import LearnMore from './LearnMore'
+
+const Cta = () => {
+  
+  return (
+    <div className='cta w-full  h-auto '>
+      <HomeTypes/>
+      <LearnMore/>
+
+    </div>
+  )
+}
+
+export default Cta
