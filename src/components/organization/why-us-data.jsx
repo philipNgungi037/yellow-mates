39ab74911dce72034514bf0b405ed7df2b8520aa
x-russian <philophilo037@gmail.com>
2023-10-07 08:02:27 +0300

    :clown_face: Mock data.

---
+++ src/components/organization/why-us-data.jsx
@@ -0,0 +1,11 @@
+const whyUsLists = [
+    ['Our prices are very affordable starting from Kshs. 199,000 only'],
+    ['Our prices are inclusive of title processing fees and legal fees, no hidden charges'],
+    ['We offer a flexible installment payment plan of up to 12 months.'],
+    ['Title deed delivery – we do all the paperwork for you and deliver title deeds within 6 – 12 months upon completion of payment.'],
+    ['When you invest with Username, you are assured of reaping capital gains. We have a research team that identifies places with high potential for growth near upcoming infrastructural developments such as highways, bypasses, railways, airports and cities.'],
+    ['Our properties are value-added — We ensure we have a perimeter fence, estate gate, internal access roads graded, water / borehole and electricity installed making your property ready for immediate development.'],
+    ['With over 16,000 title deeds issued, you are assured that you are working with a trusted real estate investment partner.']
+  ];
+  
+  export default whyUsLists;
