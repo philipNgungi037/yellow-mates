352420e859f314a32aa857febd76e4e0d6503545
x-russian <philophilo037@gmail.com>
2023-10-09 09:25:07 +0300

    :sparkles: Carousel-images data.

---
+++ src/components/hero-section/CarouselImages.jsx
@@ -0,0 +1,26 @@
+const CarouselImages = [
+    {
+      imageUrl: "assets/images/feat-img1.jpeg",
+      title: "The Big Karumaindo Plots",
+      description: "Situated along embu-meru highway",
+      offer: "discounted for 1.5m cash"
+    },
+    {
+      imageUrl: "assets/images/feat-img2.jpg",
+      title: "High, Green and Fertile Lands for Agriculture ",
+      description: "@Embu Mutunduri"
+    },
+    {
+      imageUrl: "assets/images/feat-img3.jpg",
+      title: "Dairy Farms and Wet Lands for SALE",
+      description: "Visit our lands, call; 0114605981",
+      image2Url :"assets/images/feat-img1.jpeg"
+    }
+    // {
+    //   imageUrl: "assets/images/feat-img4.jpg",
+    //   title: "Image 4",
+    //   description: "Description for Image 4"
+    // }
+   
+  ];
+  export default CarouselImages;
