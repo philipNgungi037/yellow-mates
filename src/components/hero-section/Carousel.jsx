fa45334995db9ab341507d1030a6b9e716e2945a
x-russian <philophilo037@gmail.com>
2023-10-09 09:24:35 +0300

    :recycle::fire::art::lipstick: Refactor code, remove some codes, restructure and abit of stylings.

--- src/components/hero-section/Carousel.jsx
+++ src/components/hero-section/Carousel.jsx
@@ -1,29 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import CarouselImages from "./CarouselImages";
 
 const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const images = [
-    'assets/images/feat-img1.jpg',
-    'assets/images/feat-img2.jpg',
-    'assets/images/feat-img3.jpeg',
-    // Add more image URLs as needed
-  ];
+
+
 
   // Function to go to the next image
   const nextImage = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    setCurrentIndex(prevIndex => (prevIndex + 1) % CarouselImages.length);
   };
 
   // Function to go to the previous image
   const prevImage = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    setCurrentIndex(
+      prevIndex => (prevIndex === 0 ? CarouselImages.length - 1 : prevIndex - 1)
     );
   };
 
-  // Automatically move to the next image every 1 second
+  // Automatically move to the next image every 1... secs
   useEffect(() => {
-    const interval = setInterval(nextImage, 10798);
+    const interval = setInterval(nextImage, 5900);
 
     // Clear the interval when the component unmounts
     return () => {
@@ -33,12 +30,34 @@ const Carousel = () => {
 
   return (
     <div className="carousel w-full ">
-     
-      <img src={images[currentIndex]} alt='' className='carousel-img w-full'/>
-     <div className='carousel-buttons flex justify-between '>
-     <button onClick={nextImage}><ion-icon name="chevron-back-outline"></ion-icon></button>
-      <button onClick={prevImage}><ion-icon name="chevron-forward-outline"></ion-icon></button>
-     </div>
+      <img
+        src={CarouselImages[currentIndex].imageUrl}
+        alt={CarouselImages[currentIndex].title}
+        className="carousel-img w-full relative bg-cover "
+      />
+      <div className="carousel-img-details absolute top-32 text-black px-0 md:px-48 text-center w-full ">
+        <h1 className="carousel-img-title text-3xl md:text-4xl">
+          {CarouselImages[currentIndex].title}
+        </h1>
+        <p className="carousel-img-location">
+          {CarouselImages[currentIndex].description}
+        </p>
+        <p className="carousel-img-offer text-white">
+        {CarouselImages[currentIndex].offer}
+        </p>
+        {/* <img src={CarouselImages[currentIndex].image2Url} alt="" className="w-full"/> */}
+      </div>
+      {/* <div className="w-full why-us-label mt-8 h-48 absolute top- p-4 bg-yellow-400">
+
+      </div> */}
+      <div className="carousel-buttons text-2xl sm:text-4xl flex justify-between ">
+        <button onClick={nextImage}>
+          <ion-icon name="chevron-back-outline" />
+        </button>
+        <button onClick={prevImage}>
+          <ion-icon name="chevron-forward-outline" />
+        </button>
+      </div>
     </div>
   );
 };
