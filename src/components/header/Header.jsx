917419543e275e0ce5d3fedd6b31f5d0c514336c
x-russian <philophilo037@gmail.com>
2023-10-09 09:20:10 +0300

    :lipstick: Change bg-color to yellow.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,9 +4,9 @@ import NavList from './NavList'
 
 const Header = () => {
   return (
-    <div className='app-header overflow-hidden  sticky top-0 z-40  bg-white flex justify-between items-center '>
+    <div className='app-header overflow-hidden  sticky top-0 z-40  bg-yellow-50 flex justify-between items-center  '>
       <Logo/>
-      <NavList/>
+      <NavList/> 
     </div>
   )
 }
