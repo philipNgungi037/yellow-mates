ac151279b3f2363b14cd72a7db82f8e71abceadd
x-russian <philophilo037@gmail.com>
2023-10-09 09:20:50 +0300

    :lipstick: change header paddings.

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Logo = () => {
   return (
     <div className='logo p-2 md:p-4 items-center content-center'>
-        <h1 className='  sm:text-xs md:text-2xl lg:text-4xl'>YELLOW HOMIES</h1>
+        <h1 className='pr-8'>YELLOW HOMIES</h1>
 
     </div>
   )
