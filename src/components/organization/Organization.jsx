475491885ae8c6fab812c004fc55eef7aff1278f
x-russian <philophilo037@gmail.com>
2023-10-09 09:34:06 +0300

    :sparkles: Import and use why-use component.

--- src/components/organization/Organization.jsx
+++ src/components/organization/Organization.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Features from './Features'
 import Description from './Description'
 import Team from './Team'
+import WhyUs from './WhyUs'
 
 const Organization = () => {
   return (
     <div className='organization w-full h-auto'>
+        <WhyUs/>
         <Features/>
         <Team/>
         <Description/>
