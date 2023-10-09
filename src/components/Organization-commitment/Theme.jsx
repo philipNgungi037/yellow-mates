a646a861b23e16bf6fadccea4fcb6e872b24852b
x-russian <philophilo037@gmail.com>
2023-10-09 09:37:55 +0300

    :lipstick: Stylings.

--- src/components/Organization-commitment/Theme.jsx
+++ src/components/Organization-commitment/Theme.jsx
@@ -4,7 +4,7 @@ import Message from './Message'
 const Theme = () => {
   return (
     <div className='theme-container w-full  relative '>
-        <img src='./assets/images/feat-img1.jpg' alt='' className='theme-img w-full '>
+        <img src='./assets/images/feat-img4.jpg' alt='' className='theme-img w-full '>
 
         </img>
         <Message/>
