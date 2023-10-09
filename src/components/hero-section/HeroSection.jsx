1d48699d3d650e20876551d74b25e3de850792f9
x-russian <philophilo037@gmail.com>
2023-10-09 09:25:36 +0300

    :recycle: Refactor some codes.

--- src/components/hero-section/HeroSection.jsx
+++ src/components/hero-section/HeroSection.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import Search from './Search'
 import Carousel from './Carousel'
 
 const HeroSection = () => {
   return (
-    <div className='hero-section w-full h-auto mt-2 flex flex-col items-center content-center   '>
-        <h2 className='absolute px-4 text-center'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-         eiusmod tempor incididunt ut labore et dolore magna aliqua <span className=''><Search/></span> </h2>
+    <div className='hero-section w-full h-auto mt-2 '>
+        {/* <h2 className='carousel-header absolute text-center '> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+         eiusmod tempor incididunt ut labore et dolore magna aliqua <span className=''><Search/></span> </h2> */}
+         
         <Carousel/>
-        
     </div>
   )
 }
