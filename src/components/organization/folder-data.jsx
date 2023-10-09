06c6c9143e6ef35772fc437fff245d5bfb8dabca
x-russian <philophilo037@gmail.com>
2023-10-09 09:33:00 +0300

    :sparkles: Add data to folder's data file.

---
+++ src/components/organization/folder-data.jsx
@@ -0,0 +1,19 @@
+const imageData = [
+  {
+    image: 'assets/images/feat-img1.jpeg',
+    category: 'Development',
+    description: 'Process that involves the purchase of raw land, rezoning, construction and renovation of buildings, and sale or lease of the finished product to end users.',
+  },
+  {
+    image: 'assets/images/feat-img2.jpg',
+    category: 'Lending',
+    description: 'Lenders play a major role in the industry as virtually all properties and developments use leverage (debt) to finance their business. ',
+  },
+  {
+    image: 'assets/images/feat-img3.jpeg',
+    category: 'Property Management',
+    description: 'Property management firms help real estate owners rent out the units in their buildings. ',
+  },
+];
+
+export default imageData;
