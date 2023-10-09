b10ed13549f979fbe5b54d6f70c6fe14551a246f
x-russian <philophilo037@gmail.com>
2023-10-09 09:31:01 +0300

    :pencil2::lipstick: Change title and change bg-color.

--- src/components/homes/HomeTypes.jsx
+++ src/components/homes/HomeTypes.jsx
@@ -9,12 +9,12 @@ const HomeTypes = () => {
 
   return (
     <div className="home-types p-4 sm:p-16">
-      <h2>Home Types</h2>
+      <h2>Search Land and their Types</h2>
       <ul className='sm:flex items-baseline justify-between gap-4 space-y-4'>
         {uniqueTypes.map((type, index) => (
           <li key={index}>
             <a href='home-types-link'>
-            <div className="home-type relative ">
+            <div className="home-type relative  shadow-lg sm:shadow-none shadow-yellow-400 ">
               <img src={homes.find((home) => home.type.title === type).type.image} alt='' className=' w-full sm:w-96  h-56'/>
                <label className='home-type-label absolute items-center  bottom-4 text-black p-2 text-center  w-1/2   '>{type}</label> 
             </div>
