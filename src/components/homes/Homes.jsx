bed612d5cce7e7d29afa30bb4209223e9aeb3f4c
x-russian <philophilo037@gmail.com>
2023-10-09 09:29:10 +0300

    :sparkles::art::lipstick: Add search component to land-listings and abit of stylings and structuring.

--- src/components/homes/Homes.jsx
+++ src/components/homes/Homes.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import homes from './homesData';
+import Search from '../hero-section/Search';
 
 const Homes = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
@@ -27,7 +28,10 @@ const Homes = () => {
 
   return (
     <div className='homes-container relative h-auto p-4 sm:p-16'>
-      <h2 className='text-center'>Yellow-Homies Top Listings</h2>
+      <div className='py-8'>
+      <h2 className='text-center text'>AFFORDABLE PLOTS FOR SALE WITHIN NAIROBI & kiambu</h2>
+      <p>We offer affordable and genuine land for sale along Kangundo Road ,Eastern Bypass, Mombasa Road and along Thika Road with Ready Title Deeds..<span><Search/></span></p>
+      </div>
       <div className='homes-scroll-container'>
         <div className='homes-wrapper grid grid-cols-1 sm:grid-cols-3 gap-4 '>
           {visibleHomes.map((home, index) => (
@@ -55,6 +59,9 @@ const Homes = () => {
         >
           <ion-icon name="arrow-back-outline"></ion-icon>
         </button>
+
+        {/**search-bar */}
+        <button>View All</button>
         <button
           className='scroll-right'
           onClick={scrollRight}
