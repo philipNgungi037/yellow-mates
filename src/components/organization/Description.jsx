6defcd89992813c253aea933ec852ca9ed20a80d
x-russian <philophilo037@gmail.com>
2023-10-09 09:31:28 +0300

    :pencil2: Changes on description.

--- src/components/organization/Description.jsx
+++ src/components/organization/Description.jsx
@@ -3,10 +3,15 @@ import React from 'react'
 const Description = () => {
   return (
     <div className='description m-2 p-16' >
-        <h2 className=' '> Nam placerat neque in ultrices placerat</h2>
-        <p>Nam et suscipit lacus, at vehicula lacus. Suspendisse potenti. Praesent scelerisque 
-          urna non quam varius interdum non vel elit. Nulla commodo sollicitudin ex, non eleifend nisl molestie eget. Etiam varius ligula vel posuere venenatis. Mauris sit amet enim in lacus vulputate laoreet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet euismod eros, a pretium nisl. Praesent egestas mauris maximus ultricies fermentum. Nunc at lacinia turpis. Praesent tincidunt ullamcorper orci, eget lacinia sapien ullamcorper sit amet. 
-          Nam placerat neque in ultrices placerat. Phasellus ut quam risus.</p>
+        <h2 className=' '> Yellow Homies land and properties group</h2>
+        <p>Yellow Homies land and properties group is a top-producing real estate team 
+          serving the greater <strong className='bg-yellow-200'> Mt Kenya</strong> region for over 10 years. With a focus on all-inclusive 
+          service and specializing in luxury, waterfront, and residential markets, the team
+          brings years of expertise and dedication to their client's success. 
+          Over the last decade, Yellow Homies land and properties group has established
+          long-term relationships with clients as part of the team's ongoing commitment to delivery.
+          The team approach allows each member to focus on their strengths providing clients with unsurpassed excellence.
+        </p>
     </div>
   )
 }
