99519d07b4ed9bdf8829d8b7f325830d6863a412
x-russian <philophilo037@gmail.com>
2023-10-10 14:55:05 +0300

    :lipstick: Remove margins.

--- src/components/social-proof/SocialCarousel.jsx
+++ src/components/social-proof/SocialCarousel.jsx
@@ -18,7 +18,7 @@ const SocialCarousel= () => {
   }, []);
 
   return (
-    <div className="social-carousel w-full h-full absolute mx-0 bg-black md:mx-72">
+    <div className="social-carousel w-full h-full absolute ">
       <div className="carousel-paragraph p-0 md:p-16 m-2 sm:p-4 bg-yellow-100 z-50">
         <h1>About Clients </h1>
         {carouselParagraphs.map((paragraph, index) => (
