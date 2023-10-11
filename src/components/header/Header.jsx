405e37a01ecf6328dfa746fe222a443f72478eda
x-russian <philophilo037@gmail.com>
2023-10-12 00:36:34 +0300

    :Recycle::art: Re-declare logo variable, to be used as prop.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,9 +3,12 @@ import Logo from './Logo'
 import NavList from './NavList'
 
 const Header = () => {
+  const logo = 'YELLOW MATES';
   return (
     <div className='app-header overflow-hidden  sticky top-0 z-40  bg-yellow-50 flex justify-between items-center  '>
-      <Logo/>
+      <Logo 
+      logo ={logo}
+      />
       <NavList/> 
     </div>
   )
--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Logo = () => {
-  const logo = 'YELLOW MATES';
+const Logo = ({logo}) => {
+ 
   return (
     <div className='logo p-2 md:p-4 items-center content-center'>
+
+      {/**logo as prop */}
         <h1 className='pr-8'>{logo}</h1>
 
     </div>
