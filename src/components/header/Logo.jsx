ec61a8cd064151e92fb38dfe15396691d330663a
x-russian <philophilo037@gmail.com>
2023-10-11 23:47:10 +0300

    :recycle::art: Create a logo variable for header.

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Logo = () => {
+  const logo = 'YELLOW MATES';
   return (
     <div className='logo p-2 md:p-4 items-center content-center'>
-        <h1 className='pr-8'>YELLOW MATES</h1>
+        <h1 className='pr-8'>{logo}</h1>
 
     </div>
   )
