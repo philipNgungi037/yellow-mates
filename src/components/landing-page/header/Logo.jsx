9898a4155905fc1a9410b8cc6b46edab4a326d41
x-russian <philophilo037@gmail.com>
2023-12-03 10:38:06 +0300

    :lipstick: Adjust on styles to logo

--- src/components/landing-page/header/Logo.jsx
+++ src/components/landing-page/header/Logo.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Logo = ({logo}) => {
  
@@ -6,7 +7,9 @@ const Logo = ({logo}) => {
     <div className='logo p-2 md:p-4 items-center content-center'>
 
       {/**logo as prop */}
-        <h1 className='pr-8'>{logo}</h1>
+      <Link to='/'>
+        <h1 className='pr-8 hover:text-yellow-100'>{logo}</h1>
+        </Link>
 
     </div>
   )
