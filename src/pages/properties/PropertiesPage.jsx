58c77607ebaae9225bc43f3bad3bcd86854aefe2
x-russian <philophilo037@gmail.com>
2023-12-03 11:24:26 +0300

    :sparkles: Properties main page that imports lists component.

---
+++ src/pages/properties/PropertiesPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropertiesList from '../../components/properties/PropertiesList'
+
+const PropertiesPage = () => {
+  return (
+    <div>
+        <PropertiesList/>
+    </div>
+  )
+}
+
+export default PropertiesPage
