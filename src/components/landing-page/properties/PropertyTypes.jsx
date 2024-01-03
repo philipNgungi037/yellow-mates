e6d737f986823e4c846de2c682ae598f965e01af
philipNgungi037 <philipngungi037@gmail.com>
2024-01-03 18:14:16 +0300

    :recyle: Rename some files, add ids to data + remove some files.

---
+++ src/components/landing-page/properties/PropertyTypes.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import homes from './propertiesData';
+
+const PropertyTypes = () => {
+
+    
+  // Extract unique home types
+  const uniqueTypes = [...new Set(homes.map((home) => home.type.title))];
+
+  return (
+    <div className="home-types p-4 sm:p-16">
+      <h2>Search Land and their Types</h2>
+      <ul className='sm:flex items-baseline justify-between gap-4 space-y-4'>
+        {uniqueTypes.map((type, index) => (
+          <li key={index}>
+            <a href='home-types-link'>
+            <div className="home-type relative  shadow-lg sm:shadow-none shadow-yellow-400 ">
+              <img src={homes.find((home) => home.type.title === type).type.image} alt='' className=' w-[600px] h-[400px]'/>
+               <label className='home-type-label absolute items-center  bottom-4 text-black p-2 text-center  w-1/2   '>{type}</label> 
+            </div>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PropertyTypes;
--- src/components/properties/PropertiesList.jsx
+++ src/components/properties/PropertiesList.jsx
@@ -3,9 +3,10 @@
 import React, { useState } from "react";
 import Header from "../landing-page/header/Header";
 import Footer from "../landing-page/footer/Footer";
-import homes from "../landing-page/homes/homesData";
+import homes from "../landing-page/properties/propertiesData";
 import Pagination from "../re-usable-components/Pagination";
 import './properties-list.css'
+import { Link } from "react-router-dom";
 
 
 const PropertiesList = () => {
@@ -28,6 +29,7 @@ const PropertiesList = () => {
       </>
       <div className="bg-white grid md:grid-cols-2 gap-4 lg:gap-8 space-y-8 md:space-y-0 mt-8 md:mt-0 w-full m-auto p-0 md:p-8 lg:p-16">
         {currentHomes.map((home, index) => (
+           <Link to={`/property/${home.id}`} key={home.id}>
           <div key={index} className="h relative text-xs sm:text-sm">
             <img src={home.type.image} alt={home.type.title} className="w-full h-[300px]" />
             <div className="bg-yellow-400 w-full absolute sm:relative lg:absolute bottom-0">
@@ -40,9 +42,10 @@ const PropertiesList = () => {
               <p className="col-span-2 border-t">{home.description}</p>
             </div>
           </div>
+          </Link>
         ))}
       </div>
-      <Pagination  pageCount={Math.ceil(homes.length / itemsPerPage)} onPageChange={handlePageChange} />
+      <Pagination pageCount={Math.ceil(homes.length / itemsPerPage)} onPageChange={handlePageChange} />
       <p className="text-center text-gray-600">
         Showing {indexOfFirstItem + 1} to {Math.min(indexOfLastItem, homes.length)} of {homes.length} items
       </p>
