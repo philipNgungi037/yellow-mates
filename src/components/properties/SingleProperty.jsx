4a19825bfb546e7e55cd4bf5a3ef9d2dff41a86e
philipNgungi037 <philipngungi037@gmail.com>
2024-01-04 14:03:53 +0300

    :recycle::fire: Refactor and remove some functions + change background.

--- src/components/properties/SingleProperty.jsx
+++ src/components/properties/SingleProperty.jsx
@@ -1,21 +1,11 @@
 // SinglePropertyPage.js
 
 import React from 'react';
-import { useParams } from 'react-router-dom';
-import homes from "../landing-page/properties/propertiesData";
+import BookSite from '../re-usable-components/forms/BookSite';
 
-// this component uses (useParams) to display each property/home/item in a single page at a time,
-const SingleProperty = () => {
-  const { id }  = useParams();
-  const singleProperty = homes.find((property) => property.id === id);
-  console.log(singleProperty)
-
-  if (!singleProperty) {
-    return <div>Property not found</div>;
-  }
-
+const SingleProperty = ({singleProperty}) => {
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className=" mx-auto px-4 py-8 bg-white">
       <div className="flex flex-col md:flex-row">
         <img src={singleProperty.type.image} alt={singleProperty.type.title} className="md:w-1/3 rounded-lg shadow-md" />
         <div className="md:ml-8">
@@ -25,9 +15,12 @@ const SingleProperty = () => {
           <p className="text-gray-700 mb-4">{singleProperty.location}</p>
           <h2 className="text-xl font-bold mb-2">Price:</h2>
           <p className="text-gray-700 mb-4">{singleProperty.price}</p>
-          <button className="border border-black text-white font-bold py-2 px-4 rounded-full">
+          <button className="border border-black  font-bold py-2 px-4 rounded-full">
             Contact Agent
           </button>
+        </div>
+        <div>
+          <BookSite />
         </div>
       </div>
     </div>
