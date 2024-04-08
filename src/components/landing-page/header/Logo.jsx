9e837133fb210d4262d53d93247b057c89a04576
philipNgungi037 <philipngungi037@gmail.com>
2024-04-08 22:03:11 +0300

    :lipstick: Add img to logo plus abit of logo-styling.

--- src/components/landing-page/header/Logo.jsx
+++ src/components/landing-page/header/Logo.jsx
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom'
 const Logo = ({logo}) => {
  
   return (
-    <div className='logo p-2 md:p-4 items-center content-center'>
+    <div className='logo p-2 md:p-4 items-center flex content-center'>
 
       {/**logo as prop */}
-      <Link to='/'>
-        <h3 className='pr-4 md:pr-8 hover:text-yellow-100 text-xs sm:text-sm md:text-xl lg:text-2xl'>{logo}</h3>
+      <Link to='/'className='flex items-center  ' >
+        <img src='assets/images/logo-x.png' alt='logo' className='h-4 sm:h-8' />
+        <h3 className='pr-4 md:pr-8  hover:text-yellow-600 text-xs '>{logo}</h3>
         </Link>
 
     </div>
