247d5fcb9bb2eff0d307a8ddff2912453aed42ab
philipNgungi037 <philipngungi037@gmail.com>
2024-04-08 17:55:13 +0300

    :art: Add a phone field in form-data

--- src/components/re-usable-components/forms/formsData.jsx
+++ src/components/re-usable-components/forms/formsData.jsx
@@ -1,6 +1,6 @@
 // formFields.js
 export const bookSiteFields = [
     { label: 'Name', name: 'name', type: 'text' },
-    { label: 'Phone', name: 'phone', type: 'tel', pattern: '[0-9]{3}-[0-9]{3}-[0-9]{4}' },
+    { label: 'Phone', name: 'phone', type: 'tel'},
   ];
   
