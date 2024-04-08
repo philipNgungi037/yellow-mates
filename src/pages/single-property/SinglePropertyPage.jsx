aa538c34ed8d60a1d7da12d8106d11e2575ef3fb
philipNgungi037 <philipngungi037@gmail.com>
2024-04-08 17:55:53 +0300

    :art: import data in single-property

--- src/pages/single-property/SinglePropertyPage.jsx
+++ src/pages/single-property/SinglePropertyPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import SingleProperty from '../../components/properties/SingleProperty'
 import { useParams } from 'react-router-dom';
-import homes from '../../components/landing-page/properties/propertiesData';
+import homes from '../../data/properties/propertiesData';
 import SimilarProperties from '../../components/properties/SimilarProperties';
 import Footer from '../../components/landing-page/footer/Footer';
 
-const allProperties = homes
+
 
 const SinglePropertyPage = () => {
+
   //  (useParams) to display each property/home/item in a single page at a time,
   const { id } = useParams();
   const singleProperty = homes.find((property) => property.id === id);
@@ -15,14 +16,14 @@ const SinglePropertyPage = () => {
     return <div>Property not found</div>;
   }
 
-
   // Filter properties with the same type
-  const similarProperties = allProperties.filter(
+  const similarProperties = homes.filter(
     (property) => property.type.title === singleProperty.type.title && property.id !== singleProperty.id
   );
 
+  // No similar properties found
   if (similarProperties.length === 0) {
-    return  <div>Property not found</div>; // No similar properties found
+    return  <div>Property not found</div>; 
   }
 
   return (
