f8d3ca0148675bd24f977a4cb11ba58bf6e1f3f0
philipNgungi037 <philipngungi037@gmail.com>
2024-04-08 17:56:44 +0300

    :sparkles::art: Create a mock data for properties.

---
+++ src/data/properties/propertiesData.jsx
@@ -0,0 +1,103 @@
+const homes = [
+    {
+      "id": "1",
+        "type": {
+        "title": "Commercial Land",
+        "image": "./assets/images/feat-img1.jpeg"
+      },
+      "location": "Along Juja",
+      "price": "$350,000",
+      "description": "A sleek and contemporary apartment in the heart of the city, with stunning skyline views."
+    },
+    {
+      id: "2",
+        "type": {
+        "title": "Commercial Land",
+        "image": "./assets/images/feat-img4.jpg"
+      },
+      "location": "Suburbia Subdivision",
+      "price": "$250,000",
+      "description": "A charming suburban house with a spacious backyard, perfect for a family."
+    },
+    {
+      id: "3",
+        "type": {
+        "title": "Agricultural Land",
+        "image": "./assets/images/feat-img2.jpg"
+      },
+      "location": "Woodland Retreat",
+      "price": "$150,000",
+      "description": "A cozy cabin nestled in the woods, providing a tranquil escape from city life."
+    },
+    {
+      id: "4",
+      "type": {
+        "title": "Agricultural Land",
+        "image": "./assets/images/feat-img3.jpg"
+      },
+      "location": "Exclusive Resort",
+      "price": "$1,200,000",
+      "description": "A lavish and spacious villa in a private resort, featuring exquisite amenities."
+    },
+    {
+      id: "5",
+      "type": {
+        "title": "Agricultural Land",
+        "image": "./assets/images/feat-img1.jpeg"
+      },
+      "location": "Historic District",
+      "price": "$3,500,000",
+      "description": "A grand and meticulously preserved mansion with a rich history, situated in a historic district."
+    },
+    {
+      id: "6",
+      "type": {
+        "title": "Agricultural Land",
+        "image": "./assets/images/feat-img2.jpg"
+      },
+      "location": "Tropical Paradise",
+      "price": "$500,000",
+      "description": "A charming beachfront bungalow with direct access to the pristine sandy beach."
+    },
+    {
+      id: "7",
+      "type": {
+        "title": "Agricultural Land",
+        "image": "./assets/images/feat-img2.jpg"
+      },
+      "location": "Alpine Retreat",
+      "price": "$300,000",
+      "description": "A picturesque mountain chalet with breathtaking views, ideal for nature enthusiasts."
+    },
+    {
+      id: "8",
+      "type": {
+        "title": "Agricultural Land",
+        "image": "./assets/images/feat-img3.jpg"
+      },
+      "location": "Urban Neighborhood",
+      "price": "$400,000",
+      "description": "A stylish and modern townhouse with easy access to urban amenities and entertainment."
+    },
+    {
+      id: "9",
+      "type": {
+        "title": "Residential Land",
+        "image": "./assets/images/feat-img3.jpeg"
+      },
+      "location": "Rural Countryside",
+      "price": "$280,000",
+      "description": "A charming country farmhouse with a spacious lot, perfect for a peaceful rural lifestyle."
+    },
+    {
+      id: "10",
+      "type": {
+        "title": "Residential Land",
+        "image": "./assets/images/feat-img1.jpeg"
+      },
+      "location": "Artsy District",
+      "price": "$600,000",
+      "description": "A trendy and open-concept loft in a vibrant artsy neighborhood, perfect for creative individuals."
+    }
+  ];
+export default homes;
