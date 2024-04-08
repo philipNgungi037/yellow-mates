b3fe6c24c0cae84a2288d8f571555549c500f624
philipNgungi037 <philipngungi037@gmail.com>
2024-04-08 17:54:30 +0300

    :lipstick: Minor color changes to book-site form.

--- src/components/re-usable-components/forms/BookSite.jsx
+++ src/components/re-usable-components/forms/BookSite.jsx
@@ -18,28 +18,26 @@ const BookSite = () => {
   };
 
   return (
-    <div className='border'>
+    <div className='border border-yellow-400 '>
       <h2>BookSite</h2>
       <form onSubmit={handleSubmit} className='m-4' >
         {bookSiteFields.map((field) => (
           <div key={field.name} className='flex flex-col gap-2 '>
             <label>
               {field.label}:
-              <input
+              <input className='border bg-yellow-400 px-2'
                 type={field.type || 'text'}
                 name={field.name}
                 value={formData[field.name] || ''}
                 onChange={handleChange}
-                pattern={field.pattern}
                 required
               />
             </label>
             <br />
-           
           </div>
         ))}
         <br />
-        <button type="submit">Submit</button>
+        <button type="submit  " className='bg-yellow-400 hover:bg-white' >Submit</button>
       </form>
     </div>
   );
